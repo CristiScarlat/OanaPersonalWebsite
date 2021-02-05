a8095c9eb3ae82e4575a093bc9fa8b5cc520209a
CristiScarlat <78.cristi@gmail.com>
2021-02-05 23:05:56 +0200

    add picture gallery view

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const admin = require('./admin');
 
 
-async function getData() {
+async function getAllAlbums() {
   let data = {};
   const dirs = await readDir(path.resolve('media/albums'));
   const files = await Promise.all(dirs.map(async dir => {
@@ -22,19 +22,32 @@ async function getData() {
   return data;
 }
 
+async function getAlbum(album) {
+  try {
+    return await getFiles(path.resolve('media/albums' + '/' + album));
+  }
+  catch (err) {
+    console.log("error", err);
+    return err;
+  }
+}
+
 router
   .route('/')
   .get(async (req, res) => {
-    let data = await getData();
+    let data = await getAllAlbums();
     res.render('home.html', data);
   });
 
 router
-.route('/pictures')
-.get(async (req, res) => {
-  let data = await getData();
-  res.render('pictures.html', data);
-});
+  .route('/pictures')
+  .get(async (req, res) => {
+    let pictures = null;
+    if (req.query.album) {
+      pictures = await getAlbum(req.query.album);
+    }
+    res.render('pictures.html', {pictures});
+  });
 
 router.use('/admin', admin);
 
